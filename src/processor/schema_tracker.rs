ccdbbe42823e60e9d525a6e39e667ffa660d2d05
vnvo <vahid.negahdari@gmail.com>
2023-08-19 17:40:31 +0300

    properly process the first sample

--- src/processor/schema_tracker.rs
+++ src/processor/schema_tracker.rs
@@ -3,6 +3,7 @@ use schema_analysis::InferredSchema;
 use serde::de::{DeserializeSeed, Deserializer};
 use serde::Deserialize;
 use std::collections::HashMap;
+use std::collections::hash_map::Entry;
 
 #[derive(Debug, Clone)]
 pub struct SchemaContainer {
@@ -12,9 +13,22 @@ pub struct SchemaContainer {
     pub resp_schema: Option<InferredSchema>,
     pub uri: String,
     pub source: String,
+    samples: u32,
 }
 
 impl SchemaContainer {
+    fn new(tnx: &RESTTransaction) -> SchemaContainer {
+        SchemaContainer {
+            req_hdr_schema: Some(serde_json::from_str(tnx.req_hdr.get()).unwrap()),
+            req_schema: Some(serde_json::from_str(tnx.req.get()).unwrap()),
+            resp_hdr_schema: Some(serde_json::from_str(tnx.resp_hdr.get()).unwrap()),
+            resp_schema: Some(serde_json::from_str(tnx.resp.get()).unwrap()),
+            uri: "".to_owned(),
+            source: "".to_owned(),
+            samples: 1,
+        }
+    }
+
     fn update_from(&mut self, tnx: &RESTTransaction) {
         let mut json_des = serde_json::Deserializer::from_str(tnx.req_hdr.get());
         self.req_hdr_schema = Option::<InferredSchema>::deserialize(&mut json_des).unwrap();
@@ -27,36 +41,41 @@ impl SchemaContainer {
 
         let mut json_des = serde_json::Deserializer::from_str(tnx.resp.get());
         self.resp_schema = Option::<InferredSchema>::deserialize(&mut json_des).unwrap();
+
+        self.samples += 1;
     }
 }
 
 pub struct SchemaTracker {
     schemas: HashMap<String, SchemaContainer>,
+    commit_after: u32,
 }
 
 impl SchemaTracker {
     pub fn new() -> SchemaTracker {
         let hm = HashMap::<String, SchemaContainer>::new();
-        SchemaTracker { schemas: hm }
+        SchemaTracker { 
+            schemas: hm,
+            commit_after: 5, 
+        }
     }
 
     pub fn update(&mut self, key: String, tnx: &RESTTransaction) {
-        self.schemas
-            .entry(key)
-            .and_modify(|sch| {
-                sch.update_from(tnx);
-            })
-            .or_insert_with(|| {
-                let resp_inferred: InferredSchema = serde_json::from_str(tnx.resp.get()).unwrap();
-                SchemaContainer {
-                    req_hdr_schema: None,
-                    req_schema: None,
-                    resp_hdr_schema: None,
-                    resp_schema: Some(resp_inferred),
-                    uri: "".to_owned(),
-                    source: "".to_owned(),
+        let e = self.schemas.entry(key);
+
+        match e {
+            Entry::Occupied(mut entry) => {
+                let sc = entry.get_mut();
+                sc.update_from(tnx);
+
+                if sc.samples > self.commit_after {
+                    self.commit_schema();
                 }
-            });
+            },
+            Entry::Vacant(vacant) => {
+                vacant.insert(SchemaContainer::new(tnx));
+            },
+        }
     }
 
     pub fn get(&self, key: &str) -> Option<&SchemaContainer> {
@@ -67,4 +86,6 @@ impl SchemaTracker {
 
         None
     }
+
+    fn commit_schema(&self) {}
 }
